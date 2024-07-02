d0e43b55c7cc4adc8f0aaa663080e5838b73b38e
Aurelien <a.sanctorum1301@gmail.com>
2024-07-02 19:40:01 +0200

    route afficher trajet

--- models/carts.js
+++ models/carts.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const tripSchema = mongoose.Schema({
+const cartSchema = mongoose.Schema({
     departure: String,
     arrival: String,
     date: Date,
     price: Number,
 });
 
-const Trip = mongoose.model('trips', tripSchema);
+const Cart = mongoose.model('carts', cartSchema);
 
-module.exports = Trip;
+module.exports = Cart;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
-
+const Booking = require('../models/bookings')
+const Trip = require('../models/trips');
+const Cart = require('../models/carts');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
+router.post('/', (req, res) => {
+  const departure = req.body.departure; 
+  const arrival = req.body.arrival; 
+  const departureDate = req.body.departureDate;
+
+  if (!departure || !arrival || !departureDate) { 
+      return res.json({ result: false, error: "Les champs de départ et d'arrivée et dates sont obligatoires" });
+  }
+  const fromDate = new Date(departureDate);
+  const toDate = new Date(fromDate);
+  toDate.setHours(23, 59, 59);
+  
+  Trip.find({
+    departure: {$regex: new RegExp(departure, "i") },
+    arrival: { $regex: new RexExp(arrival, "i") },
+    date: {
+      $gte: fromDate,
+      $lte: toDate
+    }
+  })
+  .then(data => {
+    if (data.length > 0) {
+                  res.json({ result: true, trips: data });
+    } else {
+                  res.json({ result: false, error: "Aucun trajet trouvé" });
+    }
+})
+
+})
+
+router.post('/', (req, res) => { 
+  Cart.findOne().then(data => {
+    console.log(data);
+    res.json({ })
+  })
+
+})
+
+
+
+
+  
 module.exports = router;
