f4170c6d9aecc8b726c625e7ef5660b511d86eea
jérôme <jplusquellec@gmail.com>
2024-07-03 16:04:35 +0200

    frontend

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ require('./models/connection');
 
 var indexRouter = require('./routes/index');
 
-
+const cors = require("cors");
 var app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
